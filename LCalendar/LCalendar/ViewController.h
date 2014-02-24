6e8c242b2f25ed1aa1f54279726a301a24833dc4
haowen.liang <lianghwen@live.cn>
2014-02-24 03:28:18 -0800

    [Description of Changes] 1，init of the project
    [Code review]
    Reviewer:None
    
    [Risk Assessment]
    
    [Sphere of influence]
    
    [Self-test]
    Simulator: OK

---
+++ LCalendar/LCalendar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LCalendar
+//
+//  Created by haowenliang on 14-2-24.
+//  Copyright (c) 2014年 dpsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LCalendar/LCalendar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LCalendar
+//
+//  Created by haowenliang on 14-2-24.
+//  Copyright (c) 2014年 dpsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
